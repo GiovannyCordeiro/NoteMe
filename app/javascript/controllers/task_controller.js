43eafe4b86f2110fdfaea92a27d6daf0e33fb1f0
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-01-11 18:34:13 -0300

    feat: adding task controller

---
+++ app/javascript/controllers/task_controller.js
@@ -0,0 +1,13 @@
+// app/javascript/controllers/task_controller.js
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+
+  submitOnEnter(event) {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      const form = this.element.firstElementChild;
+      form.requestSubmit();
+    }
+  }
+}
