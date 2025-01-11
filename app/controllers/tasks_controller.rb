8f79dd17eb6a5d40505e84fa4bdd0f5d3de34ebc
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-01-11 17:31:49 -0300

    feat: adding create route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,16 @@
 class TasksController < ApplicationController
   def index
   end
+
+  def create
+    @task = Task.create(task_params)
+
+    render :index
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
