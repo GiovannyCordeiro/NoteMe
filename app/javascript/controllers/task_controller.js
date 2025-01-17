e8a018bb5b1e932ee19b2f43ff779da7adccff69
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-01-17 17:17:03 -0300

    feat: first version stimuluss

--- app/javascript/controllers/task_controller.js
+++ app/javascript/controllers/task_controller.js
@@ -1,13 +1,45 @@
-// app/javascript/controllers/task_controller.js
 import { Controller } from "@hotwired/stimulus";
 
-export default class extends Controller {
+export default class extends Controller {;
+  static targets = [ "deleteZone" ]
+  static values = {
+    dragged: { type: String, default: 'algo'}
+  }
 
-  submitOnEnter(event) {
-    if (event.key === "Enter" && !event.shiftKey) {
-      event.preventDefault();
-      const form = this.element.firstElementChild;
-      form.requestSubmit();
-    }
+
+  // connect(){
+  //   this.draggedElement = null
+  // }
+
+  startDrag(event) {
+    console.log("LOG Start DraggedElement", this.draggedelement)
+    this.draggedValue = event.currentTarget.outerHTML;
+    console.log("LOG Start DraggedElement", this.draggedValue)
+  }
+
+  dragOver(event) {
+    event.preventDefault();
+  }
+
+  drop(event) {
+    event.preventDefault();
+    console.log("LOG Drop DraggedElement", this.draggedValue)
+
+    // if (deleteZone && this.draggedElement) {
+    //   const taskId = this.draggedElement.dataset.id;
+
+    //   fetch(`/tasks/${taskId}`, {
+    //     method: "DELETE",
+    //     headers: {
+    //       "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content
+    //     }
+    //   }).then(response => {
+    //     if (response.ok) {
+    //       this.draggedElement.remove();
+    //     }
+    //   });
+
+    //   this.draggedElement = null;
+    // }
   }
 }
