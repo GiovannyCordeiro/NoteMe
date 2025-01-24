42c08cfeb8a809ce963b24e09aeb17f22e641c26
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-01-24 18:55:50 -0300

    feat: adding method google

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [ :google_oauth2 ]
   has_many :tasks
+
+  def self.from_google(u)
+    create_with(uid: u[:uid], provider: "google",
+                password: Devise.friendly_token[0, 20]).find_or_create_by!(email: u[:email])
+  end
 end
